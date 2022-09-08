e2d4f7d2c2fe23f0f2d6edf2ac2aed727115fb1c
sho6ix <shota09060906@gmail.com>
2022-09-08 16:03:02 +0900

    setting-chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button, ChakraProvider } from '@chakra-ui/react'
+import theme from './theme/theme';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme='teal'>Button</Button>
+      <p>aaaaa</p>
+    </ChakraProvider>
   );
 }
 
